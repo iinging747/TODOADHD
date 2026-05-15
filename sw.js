4482c23b145663ca0e9bcfa84c8c0023e7d80b5f
iinging747 <iinging747@gmail.com>
2026-05-15 14:14:58 +0900

    Cache sticker folder assets

--- sw.js
+++ sw.js
@@ -1,15 +1,16 @@
-const CACHE_NAME = "todoadhd-sticker-sheet-v1";
+const CACHE_NAME = "todoadhd-sticker-system-v2";
 const ASSETS = [
   "./",
   "./index.html",
   "./preview.html",
   "./calendar.html",
   "./manifest.webmanifest",
-  "./public/stickers/sticker-sheet.png"
+  "./public/stickers/sticker-sheet.png",
+  "./public/sticker/sticker-sheet.png"
 ];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => Promise.all(ASSETS.map((asset) => cache.add(asset).catch(() => null)))));
   self.skipWaiting();
 });
 
