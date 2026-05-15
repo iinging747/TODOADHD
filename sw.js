0edfade954ba6d8a18da3b8f0b5e360cdb14ee8f
iinging747 <iinging747@gmail.com>
2026-05-15 20:15:18 +0900

    wire diary patch into service worker

--- sw.js
+++ sw.js
@@ -1,11 +1,20 @@
-const CACHE_NAME = "todoadhd-weather-autogps-v18";
+const CACHE_NAME = "todoadhd-diary-patch-v21";
 
 const ASSETS = [
   "./",
   "./index.html",
   "./calendar.html",
-  "./manifest.webmanifest"
+  "./manifest.webmanifest",
+  "./diary-patch.css",
+  "./diary-patch.js"
 ];
+
+function patchCalendarHtml(html) {
+  if (html.includes("diary-patch-v21")) return html;
+  return html
+    .replace("</head>", "<link rel=\"stylesheet\" href=\"./diary-patch.css?v=21\" data-diary-patch-v21>\n</head>")
+    .replace("</body>", "<script src=\"./diary-patch.js?v=21\" data-diary-patch-v21></script>\n</body>");
+}
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
@@ -17,6 +26,22 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
+  const url = new URL(event.request.url);
+  const wantsCalendar = url.pathname.endsWith("/calendar.html") || url.pathname.endsWith("/TODOADHD/") || url.pathname.endsWith("/TODOADHD");
+  if (wantsCalendar) {
+    event.respondWith(fetch(event.request, { cache: "no-store" }).then(async response => {
+      const type = response.headers.get("content-type") || "";
+      if (!type.includes("text/html")) return response;
+      const headers = new Headers(response.headers);
+      headers.set("content-type", "text/html; charset=utf-8");
+      headers.delete("content-length");
+      const patched = new Response(patchCalendarHtml(await response.text()), { status: response.status, statusText: response.statusText, headers });
+      const copy = patched.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      return patched;
+    }).catch(() => caches.match(event.request).then(cached => cached || caches.match("./calendar.html"))));
+    return;
+  }
   event.respondWith(fetch(event.request).then(response => {
     const copy = response.clone();
     caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
