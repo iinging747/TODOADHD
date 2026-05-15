a6e0a83761bebf7b7ad67c69febfef3a707710b4
iinging747 <iinging747@gmail.com>
2026-05-15 17:35:31 +0900

    cache sprite sticker sheet only

--- sw.js
+++ sw.js
@@ -1,76 +1,4 @@
-const CACHE_NAME = "todoadhd-pomodoro-focus-v10";
-
-const ENGLISH_STICKERS = [
-  "budget.png",
-  "planning.png",
-  "study.png",
-  "date.png",
-  "binge-drama.png",
-  "marketing-analysis.png",
-  "debt-payment.png",
-  "walk.png",
-  "late-night-work.png",
-  "self-praise.png",
-  "shopping.png",
-  "ai-study.png",
-  "travel-planning.png",
-  "video-editing.png",
-  "watch-movie.png",
-  "completion-stamp.png",
-  "conspiracy-research.png",
-  "journaling.png",
-  "savings.png",
-  "stock-crypto-chart.png",
-  "investing-study.png",
-  "reading.png",
-  "sleep.png",
-  "meet-friends.png",
-  "work-at-cafe.png",
-  "crypto-meeting.png",
-  "content-meeting.png",
-  "client-communication.png",
-  "write-todo-list.png",
-  "twitter-search.png",
-  "skincare.png",
-  "day-wrap-up.png"
-];
-
-const LEGACY_STICKERS = [
-  "가계부.png",
-  "계획세우기.png",
-  "공부하기.png",
-  "데이트.png",
-  "드라마 정주행.png",
-  "마케팅성과분석.png",
-  "빚갚음.png",
-  "산책하기'.png",
-  "새벽작업.png",
-  "셀프칭찬.png",
-  "쇼핑.png",
-  "에이아이공부.png",
-  "여행계획세우기.png",
-  "영상편집.png",
-  "영화보기.png",
-  "완료도장.png",
-  "음모론 정보.png",
-  "일기쓰기.png",
-  "저축.png",
-  "주식코인차트보기.png",
-  "주식코인투자공부.png",
-  "책읽기.png",
-  "취짐.png",
-  "친구만나기.png",
-  "카페에서 작업.png",
-  "코인미팅.png",
-  "콘텐츠회의.png",
-  "클라이언트커뮤니케이션.png",
-  "투두리스트 작성.png",
-  "트위터서칭.png",
-  "피부관리.png",
-  "하루마무리.png"
-];
-
-const stickerAssets = [...ENGLISH_STICKERS, ...LEGACY_STICKERS].map((file) => `./public/stickers/${file}`);
+const CACHE_NAME = "todoadhd-sprite-diary-font-v11";
 
 const ASSETS = [
   "./",
@@ -78,7 +6,7 @@ const ASSETS = [
   "./preview.html",
   "./calendar.html",
   "./manifest.webmanifest",
-  ...stickerAssets
+  "./public/stickers/sticker-sheet.png"
 ];
 
 self.addEventListener("install", (event) => {
